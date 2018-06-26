5a6539c934485f5d57b19d959179450727bf0614
Rebecca Vest <rebecca.vest@stukent.com>
2018-06-25 22:55:08 -0600

    Allow dynamic port

--- server/server.babel.js
+++ server/server.babel.js
@@ -13,7 +13,7 @@ require('dotenv').config();
 
 const app = express();
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use('/', express.static('public'));
 app.use(cookieParser(process.env.COOKIE_SECRET));
