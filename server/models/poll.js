47e9905f6e82038e1b65d8423070b3fd58689ce4
Rebecca Vest <rebecca.vest@stukent.com>
2018-06-26 14:54:27 -0600

    Specify table names in model defs

--- server/models/poll.js
+++ server/models/poll.js
@@ -1,37 +1,42 @@
 module.exports = (sequelize, DataTypes) => {
-  const Poll = sequelize.define('Poll', {
-    id: {
-      type: DataTypes.UUID,
-      primaryKey: true,
-      allowNull: false,
-    },
-    userId: {
-      field: 'user_id',
-      type: DataTypes.STRING,
-      references: {
-        model: 'users',
-        key: 'id',
+  const Poll = sequelize.define(
+    'Poll',
+    {
+      id: {
+        type: DataTypes.UUID,
+        primaryKey: true,
+        allowNull: false,
       },
-      allowNull: false,
+      userId: {
+        field: 'user_id',
+        type: DataTypes.STRING,
+        references: {
+          model: 'users',
+          key: 'id',
+        },
+        allowNull: false,
+      },
+      question: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
     },
-    question: {
-      type: DataTypes.STRING,
-      allowNull: false,
+    {
+      tableName: 'polls',
+      underscored: true,
+      paranoid: true,
+      indexes: [{
+        name: 'poll_deleted_at_index',
+        method: 'BTREE',
+        fields: ['deleted_at'],
+      }, {
+        name: 'poll_user_id_index',
+        method: 'BTREE',
+        fields: ['user_id'],
+      },
+      ],
     },
-  }, {
-    underscored: true,
-    paranoid: true,
-    indexes: [{
-      name: 'poll_deleted_at_index',
-      method: 'BTREE',
-      fields: ['deleted_at'],
-    }, {
-      name: 'poll_user_id_index',
-      method: 'BTREE',
-      fields: ['user_id'],
-    },
-    ],
-  });
+  );
   Poll.associate = ({ User, PollOption }) => {
     Poll.belongsTo(User);
     Poll.hasMany(PollOption);
