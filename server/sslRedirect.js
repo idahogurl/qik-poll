735e86ca4626bfa86214414066bab1605f9f2a86
Rebecca Vest <rebecca.vest@stukent.com>
2018-07-13 17:03:04 -0600

    Do not use HTTPs for graphql

--- server/sslRedirect.js
+++ server/sslRedirect.js
@@ -1,6 +1,5 @@
 export default function (req, res, next) {
-  const isGraphQL = false;
-  // const isGraphQL = req.url.includes('graphql');
+  const isGraphQL = req.url.includes('graphql');
   if (!isGraphQL && process.env.NODE_ENV === 'production' &&
     (!req.secure || req.headers['x-forwarded-proto'] !== 'https')) {
     const sslUrl = ['https://', req.hostname, req.url].join('');
