621060e5362709f80f275b44c8f2089e713d6f8a
Rebecca Vest <rebecca.vest@stukent.com>
2018-07-13 16:54:50 -0600

    Hide graphiQL endpoint in production

--- server/server.babel.js
+++ server/server.babel.js
@@ -5,6 +5,7 @@ import path from 'path';
 import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
 import fs from 'fs';
 import { makeExecutableSchema } from 'graphql-tools';
+import sslRedirect from './sslRedirect';
 import getUser from './getUser';
 import resolvers from './graphql/resolvers';
 import processLogin from './login';
@@ -14,19 +15,23 @@ require('dotenv').config();
 const app = express();
 
 const port = process.env.PORT || 3000;
+const env = process.env.NODE_ENV || 'development';
 
 app.use('/', express.static('public'));
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(sslRedirect());
 
 const typeDefs = fs.readFileSync(path.resolve(__dirname, 'graphql/schema.gql'), 'utf8');
 const schema = makeExecutableSchema({ typeDefs, resolvers });
 
 app.use('/graphql', getUser, graphqlExpress(req => ({ context: req.user, schema })));
-app.get('/graphiql', getUser, graphiqlExpress({
-  endpointURL: '/graphql',
-}));
+if (env === 'development') {
+  app.get('/graphiql', getUser, graphiqlExpress({
+    endpointURL: '/graphql',
+  }));
+}
 
 app.post('/auth/facebook', (req, res, next) => {
   processLogin(req, res, next);
@@ -41,13 +46,7 @@ app.use('/logout', (req, res, next) => {
 
 // Always return the main index.html, so react-router renders the route in the client
 app.get('*', (req, res) => {
-  console.error(req.headers);
-  console.log(req.secure);
-  if (!req.securee || req.headers['x-forwarded-proto'] !== 'https') {
-    res.redirect(302, `https://${req.hostname}${req.originalUrl}`);
-  } else {
-    res.sendFile(path.resolve(__dirname, '..', 'public', 'index.html'));
-  }
+  res.sendFile(path.resolve(__dirname, '..', 'public', 'index.html'));
 });
 
 app.listen(port, () => {
---
+++ server/sslRedirect.js
@@ -0,0 +1,11 @@
+module.exports = function (req, res, next) {
+  const isGraphQL = false;
+  // const isGraphQL = req.url.includes('graphql');
+  if (!isGraphQL && process.env.NODE_ENV === 'production' &&
+    req.headers['x-forwarded-proto'] !== 'https') {
+    const sslUrl = ['https://', req.hostname, req.url].join('');
+    return res.redirect(sslUrl);
+  }
+
+  return next();
+};
