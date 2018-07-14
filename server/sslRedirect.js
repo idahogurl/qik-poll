250f78c53a5e035658c387440e3c566424b67640
Rebecca Vest <rebecca.vest@stukent.com>
2018-07-13 22:56:07 -0600

    Fixed force HTTPs redirect

--- server/sslRedirect.js
+++ server/sslRedirect.js
@@ -1,6 +1,7 @@
 export default function (req, res, next) {
   if (process.env.NODE_ENV === 'production' && req.url !== '/graphql' && !req.secure) {
     const sslUrl = `https://${req.hostname}${req.url}`;
+    console.log('redirect');
     return res.redirect(301, sslUrl);
   }
 
